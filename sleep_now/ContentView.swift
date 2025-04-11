c4a19fe082b7bc6adb366ad77ede1a4e6a25ff0e
siego <xige990129@gmail.com>
2025-04-11 18:25:30 +1000

    Initial Commit

---
+++ sleep_now/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleep_now
+//
+//  Created by 张曦戈 on 2025/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleep_now/sleep_nowApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep_nowApp.swift
+//  sleep_now
+//
+//  Created by 张曦戈 on 2025/4/11.
+//
+
+import SwiftUI
+
+@main
+struct sleep_nowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
